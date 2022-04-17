176327c9dcbc6c03be3db5dcaae480092226e745
Gavin Ray <ray.gavin97@gmail.com>
2022-04-17 12:06:07 -0400

    fixes, make it compile

--- pkg/server.go
+++ pkg/server.go
@@ -1,9 +1,15 @@
 package pgspy
 
+import "os"
+
 // Start will start a new server
 func Start() {
-	pgHost := "127.0.0.1:5432"
-	proxyHost := "127.0.0.1:5543"
+	// Read DATABASE_PORT and PROXY_PORT from environment variables
+	var databasePort string = getEnv("DATABASE_PORT", "5432")
+	var proxyPort string = getEnv("PROXY_PORT", "5433")
+
+	pgHost := "0.0.0.0" + ":" + databasePort
+	proxyHost := "0.0.0.0" + ":" + proxyPort
 
 	proxy := NewProxy(pgHost, proxyHost)
 	queryWatcher := QueryWatcher{}
@@ -16,4 +22,12 @@ func Start() {
 	// 	}
 	// }
 	proxy.Start()
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
 }
